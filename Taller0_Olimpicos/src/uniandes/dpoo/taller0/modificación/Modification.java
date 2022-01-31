53982667d1a340678823f49602b29d46f26c687b
Sayala100 <88835469+Sayala100@users.noreply.github.com>
2022-01-30 22:20:17 -0500

    Add files via upload

---
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificación/Modification.java
@@ -0,0 +1,23 @@
+package uniandes.dpoo.taller0.modificación;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modification {
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException
+	
+	{
+		System.out.println("Hola, mundo!");
+		CalculadoraEstadisticas calc =  LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+		
+	}
+
+}
+	
+
+	
